847bb42e8ba5c0535fb24587ab5ffb5dd3963011
Leandro <leandroklink13@gmail.com>
2025-12-24 20:02:12 -0300

    Adicionando arrays multidimensionais para insersão de 3 notas diferentes para cada aluno, e calculo de médias individuais

--- src/javaapplication3/GradeBook.java
+++ src/javaapplication3/GradeBook.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package javaapplication3;
-import java.util.Scanner;
 /**
  *
  * @author Leandro
@@ -13,10 +12,10 @@ import java.util.Scanner;
 public class GradeBook{
     
     private String courseName;
-    private int[] grades; //array de notas de alunos
+    private int[][] grades; //array de notas de alunos
     
     //construtor de dois argumentos
-    public GradeBook ( String name, int[] gradesArray){
+    public GradeBook ( String name, int[][] gradesArray){
         courseName = name;   
         grades = gradesArray;
     }
@@ -34,7 +33,6 @@ public class GradeBook{
     }
     public void processGrades(){
         outputGrades();
-        System.out.printf("\nClass average is %.2f\n", getAverage());
         
         System.out.printf("\nLowest grade is %d\nHighest grade is %d\n\n",
                 getMinimum(), getMaximum());
@@ -42,25 +40,26 @@ public class GradeBook{
         outputBarChart();
     }
     public int getMinimum(){
-        int lowGrade = grades[0];
-        for(int grade : grades){
-            if (grade < lowGrade)
-                lowGrade = grade;
-        }
+        int lowGrade = grades[0][0];
+        for(int[] studentGrades : grades)
+            for(int grade : studentGrades)
+                if (grade < lowGrade)
+                    lowGrade = grade;
         return lowGrade;
     }
     public int getMaximum(){
-        int highGrade = grades[0];
-        for(int grade : grades)
-            if (grade > highGrade)
-                highGrade = grade;
+        int highGrade = grades[0][0];
+        for(int[] studentGrades : grades)
+            for(int grade : studentGrades)
+                if (grade > highGrade)
+                    highGrade = grade;
         return highGrade;
     }
     
-    public double getAverage(){
+    public double getAverage(int[]setOfGrades){
         int total = 0;
         
-        for (int grade : grades)
+        for (int grade : setOfGrades)
             total += grade;
         return (double) total / grades.length;
     }
@@ -68,10 +67,11 @@ public class GradeBook{
         System.out.println("Grade distribution: ");
         
         int[] frequency = new int[11];
+        for (int[] studentGrades : grades){
         
-        for (int grade : grades)
-            ++frequency[grade/10];
-        
+            for (int grade : studentGrades)
+                ++frequency[grade/10];
+        }
         for (int cont = 0; cont < frequency.length; cont++) {
             if (cont == 10)
                 System.out.printf("%5d: ", 100);
@@ -83,15 +83,23 @@ public class GradeBook{
                 System.out.print("*");
             }    
             System.out.println("");
-            
-
         }
     }
     public void outputGrades(){
         System.out.println("The grades are:\n");
+        System.out.print( "           " );
+        
+        for (int test = 0; test < grades[0].length; test++)
+            System.out.printf("Test %d: ", test + 1);
+        System.out.println("Average");
         
         for (int student = 0; student < grades.length; student++) {
-            System.out.printf("Student %2d: %3d\n", student+1, grades[student]);
+            System.out.printf("Student %2d", student + 1);
+            for (int test : grades[ student ])
+                System.out.printf("%8d", test);
+            
+            double average = getAverage(grades[student]);
+            System.out.printf("%9.2f\n", average);
         }
     }
 } 
--- src/javaapplication3/GradeBookTest.java
+++ src/javaapplication3/GradeBookTest.java
@@ -12,7 +12,14 @@ public class GradeBookTest {
     
     public static void main(String[] args){
         
-        int[] gradesArray = {87, 19, 96, 100, 50, 22, 89, 92, 87, 76, 48, 93};
+        int[][] gradesArray = {{87, 91, 100},
+                               {63, 87, 96}, 
+                               {58, 29, 100},
+                               {99, 98, 99},
+                               {78, 28 , 53},
+                               {49, 90, 01}, 
+                               {90, 96, 92},
+                               {89, 87, 82}};
         
         GradeBook myGradeBook = new GradeBook("CS101 Introductiuon to Java Promramminmg", gradesArray);
         
